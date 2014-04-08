86afeb7c5b76bdbe06a1a16e6f5347ec70ad4e2e
Cyril Rohr <cyril.rohr@gmail.com>
2014-04-08 17:23:07 +0100

    Make a request to github.

--- web.rb
+++ web.rb
@@ -1,5 +1,12 @@
 require 'sinatra'
+require 'net/http'
+require 'uri'
 
 get '/' do
-  "Hello, world"
+  uri = URI(ENV.fetch('HELLO_URL') { 'https://api.github.com/rate_limit' })
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true
+  response = http.request(Net::HTTP::Get.new(uri.request_uri))
+
+  "Hello, world. Here is the rate limiting response from Github: #{response.body.inspect}\n"
 end
